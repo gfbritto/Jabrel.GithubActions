7af5d01000e660a48fb0b028149c556eae13a44c
Gabriel Brito <gfbritto@outlook.com>
2024-03-18 22:21:19 -0300

    feat: add car and bike types

---
+++ Jabrel.GithubActions.Domain/Interfaces/IVehicle.cs
@@ -0,0 +1,9 @@
+﻿namespace Jabrel.GithubActions.Domain.Interfaces;
+
+public interface IVehicle
+{
+    void Drive();
+    void Stop();
+    void Refuel();
+    void TurnOn();
+}
--- Jabrel.GithubActions/Program.cs
+++ Jabrel.GithubActions/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//write a simple program which the user can make actions with car class
+
+using Jabrel.GithubActions.Domain;
+
+var car = new Car();
+
+Console.WriteLine("What do you want to do with the car?");
+Console.WriteLine("1. Drive");
+Console.WriteLine("2. Stop");
+Console.WriteLine("3. Refuel");
+Console.WriteLine("4. Turn on");
+
+var action = Console.ReadLine();
+
+switch (action)
+{
+    case "1":
+        car.Drive();
+        break;
+    case "2":
+        car.Stop();
+        break;
+    case "3":
+        car.Refuel();
+        break;
+    case "4":
+        car.TurnOn();
+        break;
+    default:
+        Console.WriteLine("Invalid action");
+        break;
+}
+
+Console.WriteLine("Press any key to exit");
